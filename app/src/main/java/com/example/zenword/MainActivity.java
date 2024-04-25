9c66544c4277960c7e5d6e54c1ba9c994e609289
David <vazquezrivasdavid@gmail.com>
2024-04-25 17:04:24 +0200

    Add logic to check if a word is solution

--- app/src/main/java/com/example/zenword/MainActivity.java
+++ app/src/main/java/com/example/zenword/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.zenword;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public boolean isSolutionWord(String word1, String word2) {
+        // TODO use our own map
+        HashMap<Character, Integer> catalogue = new HashMap<>();
+
+        for (char c : word1.toCharArray()) {
+            if (catalogue.containsKey(c)) {
+                catalogue.put(c, catalogue.get(c) + 1);
+            } else {
+                catalogue.put(c, 1);
+            }
+        }
+
+        for (char c : word2.toCharArray()) {
+            if (!catalogue.containsKey(c)) return false;
+
+            int count = catalogue.get(c);
+            if (count == 0) return false;
+
+            catalogue.put(c, count - 1);
+        }
+
+        return true;
     }
 }
